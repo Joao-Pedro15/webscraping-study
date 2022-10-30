1635b7d3b5d10b00ca60c013dddd03cf10287d52
João Pedro <joao.monteiro@veredaeducacao.com>
2022-10-29 21:03:40 -0300

    feat: learn and read documentation and examples

---
+++ main.js
@@ -0,0 +1,42 @@
+import puppeteer from 'puppeteer';
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+
+  await page.goto('https://www.google.com/')
+  await page.type('.gLFyf', 'Manchester City')
+  await page.keyboard.press('Enter')
+  await page.waitForSelector('.YIikAd')  
+  await page.click('.YIikAd')
+  await page.waitForSelector('.liveresults-sports-immersive__match-tile')
+  const tds = await page.$$eval('.liveresults-sports-immersive__match-tile', e => e.length)
+  console.log(tds)
+
+  await page.waitForTimeout(3000)
+  await browser.close();
+})();
+
+
+
+// await page.goto('https://developers.google.com/web/');
+
+// // Type into search box.
+// await page.type('.devsite-search-field', 'Headless Chrome');
+
+// // Wait for suggest overlay to appear and click "show all results".
+// const allResultsSelector = '.devsite-suggest-all-results';
+// await page.waitForSelector(allResultsSelector);
+// await page.click(allResultsSelector);
+
+// // Wait for the results page to load and display the results.
+// const resultsSelector = '.gsc-results .gs-title';
+// await page.waitForSelector(resultsSelector);
+
+// // Extract the results from the page.
+// const links = await page.evaluate(resultsSelector => {
+//   return [...document.querySelectorAll(resultsSelector)].map(anchor => {
+//     const title = anchor.textContent.split('|')[0].trim();
+//     return `${title} - ${anchor.href}`;
+//   });
+// }, resultsSelector);
